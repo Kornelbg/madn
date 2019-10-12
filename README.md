6d4abbdd2812a55b454e37d7d08f0ba5b5f55158
djackson97 <dominictobiasjackson@yahoo.de>
2019-10-12 22:17:50 +0200

    test

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # madn
+
+1 Active 	1Fenster (loggin)
+
+
+Button benutzer erstellen 1 Active 2Fenser (benutzererstllen) 
+
+Button guest 1 Active 3 Fenster (Auswahlmenu)
+
+Spiele erstellen 2 Active 1 Fenster (Spiel)
--- app/src/main/java/de/bsfreising/myapplication/MainActivity.java
+++ app/src/main/java/de/bsfreising/myapplication/MainActivity.java
@@ -2,14 +2,52 @@ package de.bsfreising.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    //buttons buttons id zuweisung
+    private Button buttonUser = findViewById(R.id.buttonUser);
+    private Button buttonGuest = findViewById(R.id.buttonGuest);
+  //  private Button buttonGuest = findViewById(R.id.buttonGuest);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
-        //test
+
+
+        //Butons onclicklistener gesetzt
+        buttonUser.setOnClickListener(this);
+        buttonGuest.setOnClickListener(this);
+       // buttonGuest.setOnClickListener(this);
+
+
+
+    }
+
+    public boolean logginDatei( String user, String passwd ) {
+
+        // gibt an ob user und eingangsdaten kombination in datei vorhanden ist
+        boolean u_p_in_datei = false;
+
+
+
+        return u_p_in_datei;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        //switch Abfrage auf id der Buttons
+
+
     }
 }
