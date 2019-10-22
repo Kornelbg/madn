426217abbd46470d10d60e9c320dbb6188ae147b
djackson97 <dominictobiasjackson@yahoo.de>
2019-10-22 20:44:54 +0200

    View´s angepasst

--- app/src/main/java/de/bsfreising/myapplication/MainActivity.java
+++ app/src/main/java/de/bsfreising/myapplication/MainActivity.java
@@ -1,26 +1,43 @@
 package de.bsfreising.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    //buttons
-    private Button buttonUser;
-    private Button buttonGuest;
-    private Button buttonLogin;
+    // Layoutes activity-main
+        //Buttons
+            private Button buttonGuest;
+            private TextView buttonUser;
+            private Button buttonLogin;
+            private Button buttonMenu;
+            private Button buttonInfo;
+            private CheckBox checkBoxRememberMe;
 
-    private Button buttonUserlogin;
-    private Button buttonUserBack;
 
-    private TextInputEditText inputUserName;
-    private TextInputEditText userInputPassword;
+    //Layouts Createuser
+        //Buttons
+            private Button buttonCreatUserBack;
+            private Button buttonUserInput;
+
+        //TextInputEditTExt
+            private TextInputEditText inputUserName;
+            private TextInputEditText userInputPassword;
+
+    //Layouts playasguest
+        //Buttons
+            private Button buttonPlayAsGuestBack;
+
+    //Layouts loggin
+        //Buttons
+            private Button buttonLoginUserBack;
+
 
 
     @Override
@@ -28,23 +45,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.activity_main);
+        startbildschirm();
 
-        //buttons id zuweisung
-        buttonUser = findViewById(R.id.buttonUser);
-        buttonGuest = findViewById(R.id.buttonGuest);
-        buttonLogin = findViewById(R.id.buttonLogin);
 
-        buttonUserlogin = findViewById(R.id.buttonUserlogin);
-        buttonUserBack = findViewById(R.id.buttonUserBack);
-
-        inputUserName = findViewById(R.id.inputUserName);
-        userInputPassword = findViewById(R.id.userInputPassword);
-
-        //Butons onclicklistener gesetzt
-        buttonUser.setOnClickListener(this);
-        buttonGuest.setOnClickListener(this);
-        buttonLogin.setOnClickListener(this);
+        //  muss in die richtige Methode!!  //
+        //buttonUserlogin = findViewById(R.id.buttonUserlogin);
+        //inputUserName = findViewById(R.id.inputUserName);
+        //userInputPassword = findViewById(R.id.userInputPassword);
 
 
 
@@ -56,27 +63,89 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
-        //switch Abfrage auf id der Buttons
+      //Abfrage Buttons activity_main
         switch (v.getId()) {
-
-            case  R.id.buttonUser:
-                setContentView(R.layout.createuser);
+            case R.id.textViewButtonUser :
+                benutzererErstellen();
                 break;
-            case R.id.buttonGuest:
-                setContentView(R.layout.playasguest);
+            case R.id.buttonGuest :
+                    // Spiel starten mit Guest Parameter
+                alsGastSpielen();
                 break;
-            case  R.id.buttonLogin:
-                setContentView(R.layout.login);
-                break;
-            case R.id.buttonUserlogin:
+            case R.id.buttonLogin :
                 login();
                 break;
-            case R.id.buttonUserBack:
-                setContentView(R.layout.activity_main);
+            case R.id.buttonMenu :
+                // Menu View erstellen + Methode
                 break;
+            case R.id.buttonInfo :
+                    //View erstellen
+                break;
+
+            // Abfrage Buttons Create User
+            case R.id.buttonCreatUserBack :
+                startbildschirm();
+                break;
+
+                //ada
+            //Abfrage Buttons playasGuest
+            case R.id.buttonPlayAsGuestBack :
+                startbildschirm();
+                break;
+
+
+
+
         }
 
 
+    }
+    public void startbildschirm() {
+        //setzen des aktiven Layouts
+        setContentView(R.layout.activity_main);
+
+
+        buttonUser = findViewById(R.id.textViewButtonUser);
+        buttonGuest = findViewById(R.id.buttonGuest);
+        buttonLogin = findViewById(R.id.buttonLogin);
+        buttonMenu = findViewById(R.id.buttonMenu);
+        buttonInfo = findViewById(R.id.buttonInfo);
+        checkBoxRememberMe = findViewById(R.id.checkBoxRememberMe);
+
+        buttonUser.setOnClickListener(this);
+        buttonGuest.setOnClickListener(this);
+        buttonLogin.setOnClickListener(this);
+        buttonMenu.setOnClickListener(this);
+        buttonInfo.setOnClickListener(this);
+
+        //checkBoxRememberMe
+    }
+
+    public void benutzererErstellen() {
+        //setzen des aktiven Layouts
+        setContentView(R.layout.createuser);
+
+/*        //TextInputEditText --> Usereingaben
+        inputUserName = findViewById(R.id.inputUserName);
+        userInputPassword = findViewById(R.id.userInputPassword);
+
+
+        //Button Speichern
+        buttonUserInput = findViewById(R.id.buttonUserInput);
+        buttonUserInput.setOnClickListener(this);
+*/
+        //Button zurück
+        buttonCreatUserBack = findViewById(R.id.buttonCreatUserBack);
+        buttonCreatUserBack.setOnClickListener(this);
+
+    }
+    public void alsGastSpielen() {
+        //setzen des aktiven Layouts
+        setContentView(R.layout.playasguest);
+
+        //Button buttonGuestback
+        buttonPlayAsGuestBack = findViewById(R.id.buttonPlayAsGuestBack);
+        buttonPlayAsGuestBack.setOnClickListener(this);
     }
 
     public void createUser() {
