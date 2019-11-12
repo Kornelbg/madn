1f7b06533936d70f55d9964066cf3860ad01e533
djackson97 <dominictobiasjackson@yahoo.de>
2019-11-12 11:42:21 +0100

    Create Info_1.java

---
+++ app/src/main/java/de/bsfreising/myapplication/Info_1.java
@@ -0,0 +1,28 @@
+package de.bsfreising.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Info_1 extends Activity implements View.OnClickListener {
+
+    private Button buttonInfoClose;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.allgemeine_info);
+
+         buttonInfoClose = findViewById(R.id.buttonInfoClose);
+         buttonInfoClose.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+}
